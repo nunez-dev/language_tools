8fbd86502267f10a9e85c2e9265b2cd88cd69487
nunez-dev <139050504+nunez-dev@users.noreply.github.com>
2023-09-24 15:34:02 +1000

    changed how to get IPA, added audio field, change file name convention

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # language_tools
 
-wiktionary_webscraper.py is a quick and dirty wiktionary web scraper to download words, IPA, and their definitions from en.wiktionary.org.
+wiktionary_webscraper.py is a quick and dirty wiktionary web scraper to download words, IPA, definitions, and audio from en.wiktionary.org.
 
-It puts them into an unsorted text file with a '|' seperator by default named something like "kazakh_verbs_23:13:02.263280.txt".
-Currently that is "word | ipa | def1 | def2 | def3".
+It puts them into an unsorted text file with a '|' seperator by default named something like "spanish_verbs_time.
+Currently that is "word | ipa | def1 | def2 | def3 | relative_audio_path".
+Audio would be stored in "spanish_audio_time"
 
-The idea is then to manually import into anki to make an anki dictionary.
-Testing has been minimal so it may or may not generalise to all languages and parts of speech.
+The hope is then to manually import them into anki to make an anki dictionary.
+Testing has been minimal so it may or may not generalise to all languages and parts of speech. Expect breaks.
 
 ```bash
 # Requires https://pypi.org/project/beautifulsoup4/
--- wiktionary_webscraper.py
+++ wiktionary_webscraper.py
@@ -52,9 +52,12 @@ words_on_current_page = 0
 number_of_defs = 3
 done = 0
 count = 0
+ipa_pattern = ""
+
 mutex = threading.Lock() # For writing to file
-max_pages = 1000000 # used for testing, lower if you only want first x pages of words
-debug_filename = "wiktionary_webscraper_" + str(datetime.now().time()) + ".html"
+max_pages = 1 # used for testing, lower if you only want first x pages of words
+time_str = datetime.utcnow().strftime('%Y-%m-%d_%H:%M:%S')
+debug_filename = "wiktionary_webscraper_" + time_str + ".html"
 
 def download_page(url):
     response = requests.get(url)
@@ -62,12 +65,10 @@ def download_page(url):
 
 print(bcolors.HEADER + __file__ + bcolors.ENDC)
 
-# These should all be functions but aren't, main starts here basically
-
 while(True):
     try:
         raw_in = input("How many threads (default:10)? ")
-        number_of_threads = int(raw_in)
+        num = int(raw_in)
     except ValueError as e:
         if(raw_in == ''):
             number_of_threads = default_number_of_threads
@@ -75,31 +76,32 @@ while(True):
         else: 
             print(e)
         continue
-    number_of_threads = default_number_of_threads
+    number_of_threads = num
     break
-        
+
+ipa_pattern = input("Matching pattern for IPA descriptions to prioritise an IPA if there are multiple.\nImplemented to be able to prioritise certain geographies.\nSome IPAs have for example (Spain) or (Latin America) before them.\nL+ or \"Latin America\" would match the latter.\n(default:\"\")? ")
 
 # Make sure page exists
 page = False
 while(not page):
     
     language = str.capitalize(input("What language? "))
-    pos = str.lower(input("What part of speech/lemma?\n(see %s_lemmas ) " % (words_url + language)))
+    pos = str.lower(input("What part of speech/lemma?\n(See %s_lemmas) " % (words_url + language)))
 
     global_words_url = words_url + language + '_' + pos
     print(global_words_url + '\n')
 
     response = download_page(global_words_url)
-
     if (response.status_code != 200):
-        print("Error: ", end="")
+        print(bcolors.FAIL + "Error: " + bcolors.ENDC, end="")
     else:
-        print("Success: ", end="")
-        page = True
+        print(bcolors.OKGREEN + "Success: " + bcolors.ENDC, end="")
+        page=True
     
     print("Url returned " + str(response.status_code))
     print(text_div)
 
+audio_dir = "./" + str.lower(language) + "_audio_" + time_str
 # Now we know this works we can keep using this url
 while(not done):
 
@@ -191,10 +193,11 @@ def check_language(node, permissable):
     return 0
 
 # Open our file to append to
-filename = str.lower(language) + '_' + str.lower(pos) + '_' + str(datetime.now().time()) + ".txt"
+filename = str.lower(language) + '_' + str.lower(pos) + '_' + time_str + ".txt"
 
 def get_definition(num, words, mutex):
     print(bcolors.OKCYAN + "DEBUG:: Starting thread " + str(num) + " for words from " + words[0] + " to " + words[len(words)-1] + bcolors.ENDC)
+    thread_prefix = str(num) + ': '
     for word in words:
         url = def_url + word.replace(" ", "_")
         response = download_page(url)
@@ -205,35 +208,104 @@ def get_definition(num, words, mutex):
         other_language = 0
         if(not language_id):
             # If there are other problems we log it anyway but assuming it is just heading issue
-            print(bcolors.FAIL + "See " + debug_filename + ". No good response from url? " + url + bcolors.ENDC)
+            print(thread_prefix + bcolors.FAIL + "See " + debug_filename + ". No good response from url? " + url + bcolors.ENDC)
             with open(debug_filename, 'w', encoding='utf-8') as file:
                 file.write(response.text)
             language_id = soup.find('span', class_="mw-page-title-main") # search from top heading, not language heading
             other_language = 1
 
         # We will do the IPA first
+        # https://en.wiktionary.org/wiki/abalanzar#Spanish
+        # Example of multiple ipas
+        # This is another example but of a dropdown. Not considered:
+        # https://en.wiktionary.org/wiki/ababillarse
+
         pronunciation_id = language_id.find_next('span', id=re.compile("Pronunciation" + r".*"))
 
-        ipas = []
+        ipa_phonemic = 0
+        ipa_phonetic = 0
+        ipa_geo_match = 0
+        ipa = ""
+        audio_path = ""
         if(pronunciation_id):
             if(check_language(pronunciation_id, other_language)):
-                print(bcolors.OKCYAN + "DEBUG:: We found a pronunciation, but not under our language, so ignore" + bcolors.ENDC)
+                print(thread_prefix + bcolors.OKCYAN + "DEBUG:: We found a pronunciation, but not under our language, so ignore" + bcolors.ENDC)
             else:
                 pronunciation_ul = pronunciation_id.find_next('ul')
+
+                # We get the first IPA we can and store it
+                # Then if there is a square bracket one, store that instead,
+                # Just sit with the first IPA until we encounter something better, so if we encounter nothing better we have the top of the list
+                # Now if geos are enabled, add the correct geo one. No problem if nothing matches
+                # After, we get from the "ipa" variable, which represents our best find
                 for pronunciation_item in pronunciation_ul.find_all('li'):
-                    try:
-                        for ipa_span in pronunciation_item.find_all('span', class_="IPA"):
-                            ipas.append(ipa_span.string)
-                            break # If you wanted to do more ipas remove this
-                    except TypeError as e:
-                        print("No IPA")
+
+                    # Just get the first one so we have something
+                    ipa_span = pronunciation_item.find('span', class_="IPA", string=re.compile('/' + r".*" + '/'))
+                    if(ipa_span and not ipa_phonemic):
+                        ipa = ipa_span.string
+                        ipa_phonemic = 1 # So we don't override on later ones
+
+                    # Now we are only matching IPAs with square brackets because of string param
+                    ipa_span = pronunciation_item.find('span', class_="IPA", string=re.compile('[' + r".*" + ']'))
+                    if(ipa_span and not ipa_phonetic):
+                        ipa = ipa_span.string
+                        ipa_phonetic = 1
+
+                    # For each IPA list item
+                    # See if it has brackets at the front with the geography, like (Latin America)
+                    
+                    geo = pronunciation_item.find('span', class_="ib-content qualifier-content")
+                    if(geo):
+                        # See if it matches our pattern if we have one
+                        match = re.search(ipa_pattern, geo.string)
+                        if(match):
+                            print(thread_prefix + bcolors.OKCYAN + "DEBUG:: IPA Match for " + word + ": " + geo.string + bcolors.ENDC)
+                            # Great, just use whatever we can from this line
+                            # Repetitive code, could be a function
+                            ipa_span = pronunciation_item.find('span', class_="IPA", string=re.compile('/' + r".*" + '/'))
+                            if(ipa_span and not ipa_phonemic):
+                                ipa = ipa_span.string
+
+                            # Now we are only matching IPAs with square brackets because of string param
+                            ipa_span = pronunciation_item.find('span', class_="IPA", string=re.compile('[' + r".*" + ']'))
+                            if(ipa_span and not ipa_phonetic):
+                                ipa = ipa_span.string
+                        # If there is no match, we will end up trying again on the next line
+
+                    # Now the audio, still under the Pronunciation header
+                    audio = pronunciation_item.find('audio')
+                    # print("NEW LOOP:")
+                    # print(pronunciation_item.prettify())
+                    if(audio):
+                        audio_filename = audio["data-mwtitle"]
+                        audio_path = audio_dir + '/' + audio_filename
+                        # examples of src attribute
+                        # //upload.wikimedia.org/wikipedia/commons/transcoded/a/ad/LL-Q1321_%28spa%29-Millars-perra.wav/LL-Q1321_%28spa%29-Millars-perra.wav.mp3
+                        # or
+                        # //upload.wikimedia.org/wikipedia/commons/a/ad/LL-Q1321_%28spa%29-Millars-perra.wav
+                        # You can see the transcoded version as well, which we aren't considering
+                        # src attribute from last source tag, looked like it was the original source not transcoded
+                        audio_url = "http://" + audio.find_all('source')[-1]["src"][2:]
+                        audio_received = download_page(audio_url)
+                        if (audio_received.status_code != 200):
+                            print(thread_prefix + bcolors.FAIL + "Error: " + audio_url + '\n' + str(audio_received.status_code) + bcolors.ENDC, end="")
+                        else:
+                            print(thread_prefix + bcolors.OKGREEN + "Success: " + audio_url + bcolors.ENDC, end="")
+                        print('\n')
+                        if not os.path.exists(audio_dir):
+                            os.makedirs(audio_dir)
+                        with open(audio_path, 'wb') as f:
+                            f.write(audio_received.content)
 
         # Now the definition
         pos_without_s = str.capitalize(pos[:-1]) # This just strips last letter e.g: Verbs to Verb
         pos_id = language_id.find_next('span', id=re.compile(pos_without_s + r".*")) # Sometimes it is Verb_3
+        if not(pos_id):
+            print(thread_prefix + bcolors.FAIL + "No definition found for " + word + " looking under " + pos_without_s + bcolors.ENDC)
         # Again make sure it's under our language
         if(check_language(pos_id, other_language)):
-            print(bcolors.OKCYAN + "DEBUG:: We found a definition, but not under our language, so ignore" + bcolors.ENDC)
+            print(thread_prefix + bcolors.OKCYAN + "DEBUG:: We found a definition, but not under our language, so ignore" + bcolors.ENDC)
             # Idk when this would ever happen, would require there to be no definition for our language
         else:
             # Find the next ordered list
@@ -250,7 +322,7 @@ def get_definition(num, words, mutex):
 
                 # Look one tag up to check if it is an unordered list, if so it is a quotation and not a definition
                 if (definition.find_parent().name == "ul"):
-                    continue;
+                    continue
                 
                 # get only the first line. This is a cheap way to avoid synonyms since they appear on the second line
                 defs.append(definition.text.partition('\n')[0])
@@ -259,22 +331,12 @@ def get_definition(num, words, mutex):
                 i += 1
                 if(i >= number_of_defs):
                     break
-        
-        # Some post processing, turn [ipa] into /ipa/
-        i = 0
-        for single_ipa in ipas:
-            ipas[i] = '/' + single_ipa[1:-1] + '/'
-            i += 1
 
-        if len(ipas) == 0:
-            final_ipa = ''
-        else:
-            final_ipa = ipas[0]
 
         word = str.capitalize(word)
 
         # Done, now create line with desired format
-        line = word + seperator + final_ipa
+        line = word + seperator + ipa
 
         for def_number in range(number_of_defs):
             # Empty def if we don't have one to use
@@ -283,8 +345,11 @@ def get_definition(num, words, mutex):
                 definition = defs[def_number]
             line = line + seperator + definition
 
-        #print(bcolors.OKBLUE + url + bcolors.ENDC)
-        print("THREAD " + str(num) + '\n' + bcolors.OKBLUE + url + bcolors.ENDC + '\n' + bcolors.OKGREEN + line + bcolors.ENDC)
+        # and audio
+        line = line + seperator + audio_path
+
+        print(thread_prefix + bcolors.OKBLUE + url + bcolors.ENDC)
+        print(thread_prefix + bcolors.OKGREEN + line + bcolors.ENDC)
 
         # write to file (thread safe)
         # can monitor with something like
